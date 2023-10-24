2b8e38c269916a934aa29cb33eb75838dcc9a804
0Shraddha <shraddhadongol12345@gmail.com>
2023-10-24 20:29:22 +0545

    input field data tracked

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Create = () => {
+
+ const [name, setName] = useState("");
+ const [phone, setPhone] = useState("");
+ const [email, setEmail] = useState("");
+ const [[job], setJob] = useState("");
+ const [id, setId] = useState("");
+
+//  function MyComponent() {
+//   const [id, setSelectedFile] = useState(null);
+
+//   const handleFileChange = (e) => {
+//     const file = e.target.files[0]; // Get the selected file
+//     setSelectedFile(file); // Store the selected file in the state
+//   };
+// }
   return (
     <>
           <h2 className="heading">Add Employee Details</h2>
@@ -15,6 +30,7 @@ const Create = () => {
             className="form-control"
             id="exampleFormControlInput1"
             placeholder="Fullname"
+            onChange={(e) => setName(e.target.value)}
             required
           />
         </div>
@@ -27,6 +43,7 @@ const Create = () => {
             className="form-control"
             id="exampleFormControlInput1"
             placeholder="Mobile Number"
+            onChange={(e) => setPhone(e.target.value)}
             required
           />
         </div>
@@ -39,6 +56,7 @@ const Create = () => {
             className="form-control"
             id="exampleFormControlInput1"
             placeholder="name@example.com"
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
         </div>
@@ -47,13 +65,13 @@ const Create = () => {
             Job Nature
           </label>
           <div class="form-check">
-  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked/>
+  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" onChange={(e) => setJob(e.target.data)} checked/>
   <label class="form-check-label" for="exampleRadios1">
    Full Time
   </label>
 </div>
 <div class="form-check">
-  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"/>
+  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"  onChange={(e) => setJob(e.target.data)} />
   <label class="form-check-label" for="exampleRadios2">
     Part Time
   </label>
@@ -61,13 +79,20 @@ const Create = () => {
         </div>
         <div class="mb-3">
   <label for="formFileSm" class="form-label">Identity Card</label>
-  <input class="form-control form-control-sm" id="formFileSm" type="file" required/>
+  <input class="form-control form-control-sm" id="formFileSm" type="file" onChange={(e) => setId(e.target.value)} required/>
 </div>
+{/* {id}
+{job}
+{name}
+{email}
+{phone} */}
         <button type="submit" className="btn btn-success">
           Submit
         </button>
       </form>
     </>
+        
+
   );
 };
 
